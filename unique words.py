f8c85c233edea407d3284cd65999c415c5a893a0
GeorgeLeto <g.krivtsov@g.nsu.ru>
2022-08-23 23:29:22 +0700

    Change a function

--- unique words.py
+++ unique words.py
@@ -1,13 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[114]:
-
-# added comment
 # предполагаю, что текст уже обработан (нет знаков препинания, переноса строк, заглавных букв)
 
 def wordProcess(txtName, encod):
-    parsedText = open(txtName, encoding = encod)
+    parsedText = open(txtName, encoding = encod) # кодировку, наверное можно убрать, у меня просто проблемы были 
     
     uniqueWords = []
     uniqueProc = []
@@ -18,35 +12,28 @@ def wordProcess(txtName, encod):
     onceCounter = 0
     wordCounter = 0
     
-    temp = ""
-    
-    for symb in parsedText.read() + " ": # тут костыль с добавлением пробела, чтобы последнее слово записывалось в массив
-        if symb != " ":
-            temp = temp + symb
-        else:
-            if not temp in uniqueWords:
-                uniqueWords.append(temp)
-                onceWords.append(temp)
+    words = parsedText.read().split(" ")
+    for word in words:
+        if not word in uniqueWords:
+                uniqueWords.append(word)
+                onceWords.append(word)
                 uniqueCounter += 1
                 uniqueProc.append(uniqueCounter)
                 onceCounter += 1
                 onceProc.append(onceCounter)
-            else:
-                uniqueProc.append(uniqueCounter)
-                if temp in onceWords:
-                    onceWords.remove(temp)
-                    onceCounter -= 1
-                    onceProc.append(onceCounter)
-                    
+        else:
+            uniqueProc.append(uniqueCounter)
+            if word in onceWords:
+                onceWords.remove(word)
+                onceCounter -= 1
+                onceProc.append(onceCounter)
+
             wordCounter += 1
-            temp = ""
+            
     parsedText.close()
     return uniqueProc, onceProc
 
 
-# In[116]:
 
 
-uP, oP = wordProcess("D:\Learning\chapters1-9\chapter1.txt", "utf8")
-print(uP, "\n"*5, oP)
 
